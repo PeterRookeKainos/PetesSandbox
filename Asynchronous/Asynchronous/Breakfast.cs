225eda213a2e8dd9ae2231bf982ba3591b217089
Peter Rooke <peter.rooke@kainos.com>
2025-07-17 16:29:10 +0100

    (Async) Async breakfast

--- Asynchronous/Asynchronous/Breakfast.cs
+++ Asynchronous/Asynchronous/Breakfast.cs
@@ -11,91 +11,87 @@ namespace Asynchronous;
 using System;
 using System.Threading.Tasks;
 
-internal class HashBrown { }
-internal class Coffee { }
-internal class Egg { }
-internal class Juice { }
-internal class Toast { }
-
 public class Breakfast
 {
+    const int DELAY = 2000; 
+    
     public void MakeBreakfast() 
     {
         Coffee cup = PourCoffee();
-        Console.WriteLine("7--> Coffee is ready");
+        Console.WriteLine("1--> Coffee is ready");
 
         Egg eggs = FryEggs(2);
-        Console.WriteLine("6--> eggs are ready");
+        Console.WriteLine("2--> Eggs are ready");
 
         HashBrown hashBrown = FryHashBrowns(3);
-        Console.WriteLine("5--> hash browns are ready");
+        Console.WriteLine("3--> Hash browns are ready");
 
         Toast toast = ToastBread(2);
         ApplyButter(toast);
         ApplyJam(toast);
-        Console.WriteLine("4--> toast is ready");
+        Console.WriteLine("4--> Toast is ready");
 
         Juice oj = PourOJ();
-        Console.WriteLine("1--> oj is ready");
+        Console.WriteLine("5--> Orange juice is ready");
         Console.WriteLine("*--> Breakfast is ready!");
     }
     
     private static Juice PourOJ()
     {
-        Console.WriteLine("1--> Pouring orange juice");
+        Console.WriteLine("5.1--> Pouring orange juice");
         return new Juice();
     }
 
     private static void ApplyJam(Toast toast) =>
-        Console.WriteLine("2--> Putting jam on the toast");
+        Console.WriteLine("4.5--> Putting jam on the toast");
 
     private static void ApplyButter(Toast toast) =>
-        Console.WriteLine("3--> Putting butter on the toast");
+        Console.WriteLine("4.4--> Putting butter on the toast");
 
     private static Toast ToastBread(int slices)
     {
         for (int slice = 0; slice < slices; slice++)
         {
-            Console.WriteLine("4--> Putting a slice of bread in the toaster");
+            Console.WriteLine("4.1--> Putting a slice of bread in the toaster");
         }
-        Console.WriteLine("4.1--> Start toasting...");
-        Task.Delay(1000).Wait();
-        Console.WriteLine("4.2--> Remove toast from toaster");
+        Console.WriteLine("4.2--> Start toasting...");
+        Task.Delay(DELAY).Wait();
+        Console.WriteLine("4.3--> Remove toast from toaster");
 
         return new Toast();
     }
 
     private static HashBrown FryHashBrowns(int patties)
     {
-        Console.WriteLine($"5--> Putting {patties} hash brown patties in the pan");
-        Console.WriteLine("5.1--> Cooking first side of hash browns...");
-        Task.Delay(1000).Wait();
+        Console.WriteLine($"3.1--> Putting {patties} hash brown patties in the pan");
+        Console.WriteLine("3.2--> Cooking first side of hash browns...");
+        Task.Delay(DELAY).Wait();
         for (int patty = 0; patty < patties; patty++)
         {
-            Console.WriteLine("5.2--> Flipping a hash brown patty");
+            Console.WriteLine("3.3--> Flipping a hash brown patty");
         }
-        Console.WriteLine("5.3--> Cooking the second side of hash browns...");
-        Task.Delay(3000).Wait();
-        Console.WriteLine("5.4--> Put hash browns on plate");
+        Console.WriteLine("3.4--> Cooking the second side of hash browns...");
+        Task.Delay(DELAY).Wait();
+        Console.WriteLine("3.5--> Put hash browns on plate");
 
         return new HashBrown();
     }
 
     private static Egg FryEggs(int howMany)
     {
-        Console.WriteLine("6--> Warming the egg pan...");
-        Task.Delay(1000).Wait();
-        Console.WriteLine($"6.1--> Cracking {howMany} eggs");
-        Console.WriteLine("6.2--> Cooking the eggs ...");
-        Task.Delay(1000).Wait();
-        Console.WriteLine("6.3--> Put eggs on plate");
+        Console.WriteLine("2.1--> Warming the egg pan...");
+        Task.Delay(DELAY).Wait();
+        Console.WriteLine($"2.2--> Cracking {howMany} eggs");
+        Console.WriteLine("2.3--> Cooking the eggs ...");
+        Task.Delay(DELAY).Wait();
+        Console.WriteLine("2.4--> Put eggs on plate");
 
         return new Egg();
     }
 
     private static Coffee PourCoffee()
     {
-        Console.WriteLine("7--> Pouring coffee");
+        Console.WriteLine("1.1--> Pouring coffee");
         return new Coffee();
     }
 }
--- Asynchronous/Asynchronous/Program.cs
+++ Asynchronous/Asynchronous/Program.cs
@@ -1,13 +1,21 @@
-﻿using Asynchronous;
+﻿using AsyncBreakfast;
+using Asynchronous;
+
+internal class HashBrown { }
+internal class Coffee { }
+internal class Egg { }
+internal class Juice { }
+internal class Toast { }
 
 public class Program
 {
     public static void Main(string[] args)
     {
         Breakfast breakfast = new Breakfast();
-        
         breakfast.MakeBreakfast();
         
+        BreakfastAsync breakfastAsync = new BreakfastAsync();
+        Task.Run(() => breakfastAsync.MakeBreakfast()).GetAwaiter().GetResult();
     }
 }
 
