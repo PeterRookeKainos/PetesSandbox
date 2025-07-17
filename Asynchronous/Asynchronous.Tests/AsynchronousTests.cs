fd6f200fcf5d9e49eb1b75cb2efb3817067d04e4
Peter Rooke <peter.rooke@kainos.com>
2025-07-17 19:05:45 +0100

    (Async) test workflow is gone

--- Asynchronous/Asynchronous.Tests/AsynchronousTests.cs
+++ Asynchronous/Asynchronous.Tests/AsynchronousTests.cs
@@ -15,7 +15,6 @@ namespace Asynchronous.Tests
             // Act
             // Using the async method to make breakfast but awaiting the result
             // This will ensure that the asynchronous operations complete before we check the result
-            // Note: The method MakeBreakfastAsync is assumed to be an async method that returns a string
             var result = await breakfastAsync.MakeBreakfast();
 
             // Assert
