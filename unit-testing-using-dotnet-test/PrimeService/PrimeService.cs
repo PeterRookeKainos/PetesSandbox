eb43903a8da5f48de0b849b9dff51d596b9a53ae
Peter Rooke <peter.rooke@kainos.com>
2025-07-22 10:22:00 +0100

    (Unit Testing) Created unit tests

--- unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
+++ unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -1,6 +1,13 @@
 ﻿namespace PrimeService;
 
-public class Class1
+public class PrimeService
 {
-
+    public bool IsPrime(int candidate)
+    {
+        if (candidate < 2)
+        {
+            return false;
+        }
+        throw new NotImplementedException("Not fully implemented.");
+    }
 }
